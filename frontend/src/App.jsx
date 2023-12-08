b665d6358bf26669eb73fbf84534e86cd9b4dcda
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-12-08 21:50:58 +0545

    Navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NabBar from "./component/NabBar";
+function App() {
+  return (
+    <div className="App">
+      <NabBar />
+    </div>
+  );
+}
+
+export default App;
