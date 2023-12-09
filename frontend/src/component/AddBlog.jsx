824e28e8494946956485bea58a0843d57c6c4abd
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-12-09 11:58:07 +0545

    making Add Blog

--- frontend/src/component/AddBlog.jsx
+++ frontend/src/component/AddBlog.jsx
@@ -31,6 +31,47 @@ const AddBlog = () => {
             placeholder="Description"
             style={{ padding: "20px" }}
           ></textarea>
+          {/* buttons */}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              margin: "10px",
+              padding: " 20px 40px",
+            }}
+          >
+            {/* top */}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginBottom: "20px",
+              }}
+            >
+              {/* file  input */}
+              <div>
+                <label htmlFor="fileInput" className="custom-file-input">
+                  Choose File
+                </label>
+                <input type="file" id="fileInput" style={{ display: "none" }} />
+              </div>
+              {/* clear  button*/}
+              <div>
+                <button>Clear</button>
+              </div>
+              {/* select tags */}
+              <div>
+                <select name="" id="">
+                  <option value="">News</option>
+                  <option value="">Entertainment</option>
+                </select>
+              </div>
+            </div>
+            {/* bottom */}
+            <div style={{ marginTop: "20px", alignSelf: "flex-start" }}>
+              <button>Post</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
