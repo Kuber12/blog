9eb656f8a4b22ef1020dc3efef9bce62053ebcc0
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-12-10 23:29:29 +0545

    Inserted new nav with router fixed
    Inserted new nav with router fixed and footer hide when clicked on create or edit

--- frontend/src/component/MyRouter.jsx
+++ frontend/src/component/MyRouter.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 import NavBar from "./NavBar";
+import NewNav from "./NewNav";
 import AddBlog from "./AddBlog";
 import EditBlog from "./EditBlog";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Footer";
 import User from "./User";
+
 const MyRouter = () => {
   const [isFooterVisible, setisFooterVisible] = useState(true);
 
   // getting data from nav and showing or hiding
-  const [dataFromChild, setDataFromChild] = useState(null);
+  // const [dataFromChild, setDataFromChild] = useState(null);
   const handeDataFromChild = (data) => {
     console.log("Data received from child:", data);
     setisFooterVisible(data);
   };
-
+  const data = (h) => {
+    console.log(h);
+  };
   return (
     <div>
       <Router>
-        <NavBar sentDataToParent={handeDataFromChild} />
+        {/* <NewNav sentDataToParent={handeDataFromChild} /> */}
+        <NewNav sentDataToParent={handeDataFromChild} />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/AddBlog" element={<AddBlog />}></Route>
--- frontend/src/component/NewNav.jsx
+++ frontend/src/component/NewNav.jsx
@@ -1,15 +1,31 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./nav.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faUser} from '@fortawesome/free-solid-svg-icons'
-const NavBar = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+const NewNav = ({ sentDataToParent }) => {
+  //logic to send data from child to parent
+  const [hideClick, sethideClick] = useState(false);
+  const [visibleClick, setvisibleClick] = useState(true);
+  const handleClickAdd = () => {
+    // Call the function passed from the parent and pass data to it
+    sentDataToParent(hideClick);
+  };
+  const handleClickVisible = () => {
+    // Call the function passed from the parent and pass data to it
+    sentDataToParent(visibleClick);
+  };
+  const sendmeg = () => {
+    sentDataToParent("Hello");
+  };
+
   return (
-    <div class="main">
+    <div className="main">
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             Blog
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -18,39 +34,55 @@ const NavBar = () => {
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
-            style={{backgroundColor:'#BDE3FF', borderColor:'black solid'}}
+            style={{ backgroundColor: "#BDE3FF", borderColor: "black solid" }}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="list">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/"
+                  onClick={handleClickVisible}
+                >
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link
+                  className="nav-link"
+                  to="/AddBlog"
+                  onClick={handleClickAdd}
+                >
                   Create
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link
+                  className="nav-link"
+                  to="/EditBlog"
+                  onClick={handleClickAdd}
+                >
                   Edit
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
-                <FontAwesomeIcon icon={faUser}/>
-                </a>
+                <Link
+                  className="nav-link"
+                  to="/User"
+                  onClick={handleClickVisible}
+                >
+                  <FontAwesomeIcon icon={faUser} />
+                </Link>
               </li>
             </ul>
-            
           </div>
         </div>
-        </nav>
+      </nav>
     </div>
   );
 };
 
-export default NavBar;
+export default NewNav;
