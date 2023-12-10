b8052d03fbe1298dfada48408afef1ea2a5c935f
666Deadsoul <sarinamaharjan234@gmail.com>
2023-12-10 21:31:06 +0545

    just a commit:

--- frontend/src/component/NavBar.jsx
+++ frontend/src/component/NavBar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-
+import React,{useState} from "react";
+import "./Nav.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
 const NavBar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="main">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            Navbar
+            Blog
           </a>
           <button
             className="navbar-toggler"
@@ -16,11 +18,12 @@ const NavBar = () => {
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            style={{backgroundColor:'#BDE3FF', borderColor:'black solid'}}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0" id="list">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
                   Home
@@ -28,63 +31,21 @@ const NavBar = () => {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/">
-                  Link
+                  Create
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="/"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
+                <a className="nav-link" href="/">
+                  Edit
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                <FontAwesomeIcon icon={faUser}/>
                 </a>
               </li>
             </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+            
           </div>
         </div>
       </nav>
