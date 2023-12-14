cfdf3da8fd16c713370b4ce421b1e37f009432d8
666Deadsoul <sarinamaharjan234@gmail.com>
2023-12-14 09:58:17 +0545

    New nav with pure css

---
+++ frontend/src/component/BestBlog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BestBlogSlider=()=>{
+    return(
+        <>
+        <div>
+            <div>
+                <h1>Best Blogs</h1>
+            </div>
+        </div>
+        </>
+    );
+};
+export default BestBlogSlider;
