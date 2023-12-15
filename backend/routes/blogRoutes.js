539d08b1616ec10bba46ed30309aea1df6ebea4d
kuber12 <kubershakya123@hotmail.com>
2023-12-15 19:45:59 +0545

    validation disabled

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {getBlogs,getBlog,createBlog,deleteBlog,editBlog} = require("../controllers/blogController");
 const validateToken = require("../middleware/validateTokenHandler");
 
-router.use(validateToken);
+// router.use(validateToken);
 router.route("/").get(getBlogs).post(createBlog);
 router.route("/:id").get(getBlog).delete(deleteBlog).put(editBlog);
 
