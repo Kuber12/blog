385953f5e4ebae34c974dab4042450596c80e47f
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2023-12-17 21:48:57 +0545

    Api used to show cards data
    Api used to show cards data. setup the server and mongodb to use it

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -2,12 +2,17 @@ const express = require("express");
 const blog = require("../models/blogModel");
 const router = express.Router();
 
-const {getBlogs,getBlog,createBlog,deleteBlog,editBlog} = require("../controllers/blogController");
-const validateToken = require("../middleware/validateTokenHandler");
+const {
+  getBlogs,
+  getBlog,
+  createBlog,
+  deleteBlog,
+  editBlog,
+} = require("../controllers/blogController");
+// const validateToken = require("../middleware/validateTokenHandler");
 
-router.use(validateToken);
+// router.use(validateToken);
 router.route("/").get(getBlogs).post(createBlog);
 router.route("/:id").get(getBlog).delete(deleteBlog).put(editBlog);
 
-
-module.exports = router
+module.exports = router;
