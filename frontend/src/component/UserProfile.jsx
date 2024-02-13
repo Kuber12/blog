88dad238defae6f9902a015f8593bfdedfd9d811
kuber12 <kubershakya123@hotmail.com>
2024-02-13 10:11:34 +0545

    user profile css updatd

--- frontend/src/component/UserProfile.jsx
+++ frontend/src/component/UserProfile.jsx
@@ -34,7 +34,7 @@ const UserProfile = () => {
   return (
     <>
       <div id="mainBody">
-        <div className="btwnBlc">
+        {/* <div className="btwnBlc">
           <div className="Profile">
             <img src={imgUrl} alt="userprofile" />
           </div>
@@ -60,19 +60,48 @@ const UserProfile = () => {
               <p className="udP">{email}</p>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="btmBlc">
-          <div className="bio">
-            {Error != "" && <h6>{Error}</h6>}
-            <p id="fname"> {name ? name : ""} </p>
-            <p id="uname">@{username ? username : ""}</p>
-            {/* bio form ma add vako xaina */}
-            <p id="bioo">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
-              nobis ipsum temporibus laboriosam recusandae odio, id eveniet
-              commodi aspernatur, velit tempora distinctio soluta rem, dolorum
-              aliquam ratione illum totam ea?
-            </p>
+          <div className="userprofile-top-container">
+            <div className="profile-follow">
+              <div className="Profile">
+                <img src={imgUrl} alt="userprofile" />
+              </div>
+              <div className="follow_me">
+                <button className="Fbtn">Follow Me +</button>
+              </div>
+            </div>
+            <div className="bio">
+              {Error != "" && <h6>{Error}</h6>}
+              <p id="fname"> {name ? name : ""} </p>
+              <p id="uname">@{username ? username : ""}</p>
+              {/* bio form ma add vako xaina */}
+              <p id="bioo">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
+                nobis ipsum temporibus laboriosam recusandae odio, id eveniet
+                commodi aspernatur, velit tempora distinctio soluta rem, dolorum
+                aliquam ratione illum totam ea?
+              </p>
+            </div>
+            <div className="aboutMe">
+              <img src={abm} id="im" />
+              <div className="User_Dtl">
+                <FontAwesomeIcon icon={faCakeCandles} className="ics" />
+                <p className="udP">2000.04.10</p>
+              </div>
+              <div className="User_Dtl">
+                <FontAwesomeIcon icon={faVenus} className="ics" />
+                <p className="udP">Female</p>
+              </div>
+              <div className="User_Dtl">
+                <FontAwesomeIcon icon={faHome} className="ics" />
+                <p className="udP">Kentucky, U.S.A</p>
+              </div>
+              <div className="User_Dtl">
+                <FontAwesomeIcon icon={faEnvelope} className="ics" />
+                <p className="udP">{email}</p>
+              </div>
+            </div>
           </div>
           <div className="myPost">
             <p className="Ht">My Post</p>
