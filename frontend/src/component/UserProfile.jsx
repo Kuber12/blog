f9576ceff64bc2191804987e149aff33afbdbedc
kuber12 <kubershakya123@hotmail.com>
2024-02-14 17:58:22 +0545

    minor css fix

--- frontend/src/component/UserProfile.jsx
+++ frontend/src/component/UserProfile.jsx
@@ -69,7 +69,7 @@ const UserProfile = () => {
                 <img src={imgUrl} alt="userprofile" />
               </div>
               <div className="follow_me">
-                <button className="Fbtn">Follow Me +</button>
+                <button className="blog-user-follow">Follow Me +</button>
               </div>
             </div>
             <div className="bio">
