42bfd7afa8c4f7af2d8bbe1f065aad9641e984fc
Rikin07 <110732947+Senyadt2@users.noreply.github.com>
2024-02-19 20:14:35 +0545

    env setup

--- frontend/src/component/firebase.js
+++ frontend/src/component/firebase.js
@@ -1,17 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6H1dZmMI8V02nyhvmdh8lMbJa_87oOOs",
-  authDomain: "uploadingfile-fdbcf.firebaseapp.com",
-  projectId: "uploadingfile-fdbcf",
-  storageBucket: "uploadingfile-fdbcf.appspot.com",
-  messagingSenderId: "189935990055",
-  appId: "1:189935990055:web:f1dc15daf911887f246502",
+  // apiKey: "AIzaSyB6H1dZmMI8V02nyhvmdh8lMbJa_87oOOs",
+  // authDomain: "uploadingfile-fdbcf.firebaseapp.com",
+  // projectId: "uploadingfile-fdbcf",
+  // storageBucket: "uploadingfile-fdbcf.appspot.com",
+  // messagingSenderId: "189935990055",
+  // appId: "1:189935990055:web:f1dc15daf911887f246502",
+
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
